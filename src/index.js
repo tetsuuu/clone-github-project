10d68191a84cd1fa0650bf226e9c49f56c43a886
ttsknst <tkinoshita29@gmail.com>
2020-05-21 11:53:17 +0900

    show originData

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const generateProjectMutation = require('./generate_project_mutation');
 
         console.log('JSON.stringify(originData)');
         debug(JSON.stringify(originData));
-        console.log('originData : ' + originData);
+        console.log('originData : ' + JSON.stringify(originData));
 
         // Generate Project board cloning mutation query from preview project
         console.log('third step');
