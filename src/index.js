fb20cb7817426a9cca8e47c9c76add801b69e09b
ttsknst <tkinoshita29@gmail.com>
2020-05-21 11:26:28 +0900

    add log to end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { getInput, debug, setFailed } = require('@actions/core');
 const github = require('@actions/github');
-const { moment } = require('moment');
+const moment = require('moment');
 
 const getRepositoriesQuery = require('./get_repositories_query');
 const generateProjectMutation = require('./generate_project_mutation');
@@ -29,24 +29,38 @@ const generateProjectMutation = require('./generate_project_mutation');
 
         console.log('debug(prevProject)');
         debug(prevProject);
+        console.log('prevProject : ' + prevProject);
 
         console.log('await octokit.graphql(prevProject)');
         const {originData} = await octokit.graphql(prevProject);
 
         console.log('JSON.stringify(originData)');
         debug(JSON.stringify(originData));
+        console.log('originData : ' + originData);
 
         // Generate Project board cloning mutation query from preview project
+        console.log('third step');
+
         const date = moment();
         const startDate = date.add(4, 'day').format('MM/DD');
         const endDate = date.add(9, 'day').format('MM/DD');
         const body = startDate + endDate;
+
+        console.log('body is ' + body);
+
         const prevNum = originData.repository.projects.nodes.name.replace(/[^0-9]/g, '');
         const number = int(prevNum) + 1;
         const name = `Openβ ${number}th batch`;
+
+        console.log('name is ' + name);
+
         const id = originData.data.repository.projects.nodes.id;
         const ownerId = originData.data.repository.projects.nodes.owner.id;
+        console.log('id is ' + id);
+        console.log('ownerId is ' + ownerId);
 
+
+        console.log('fourth step');
 		const projectMutation = generateProjectMutation(body, name, id, ownerId);
 
 		debug(projectMutation);
